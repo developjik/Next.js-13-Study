16be326911cc79c92ad3b1ef9388d4ce25211de0
developjik <developjik@gmail.com>
2024-04-29 01:46:10 +0900

    feat: add skeleton component

---
+++ packages/ui/components/skeleton/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@repo/utils";
+
+export type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
+
+const Skeleton = ({ className, ...props }: SkeletonProps) => {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  );
+};
+
+export { Skeleton };
