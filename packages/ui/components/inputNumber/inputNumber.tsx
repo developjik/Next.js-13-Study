4916c8f3610746a6534a892bfd03e3ab50785f81
developjik <developjik@gmail.com>
2024-05-06 03:09:00 +0900

    feat: add inputNumber component

---
+++ packages/ui/components/inputNumber/inputNumber.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { cn } from "@repo/utils";
+
+export type InputNumberProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  decimalPlaces?: number;
+  min?: number;
+  max?: number;
+};
+
+const parseNumber = (value: string) => value.replace(/,/g, "");
+const formatNumber = (value: string) =>
+  value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+const InputNumber = React.forwardRef<HTMLInputElement, InputNumberProps>(
+  ({ className, type, decimalPlaces = 0, min, max, ...props }, ref) => {
+    const [displayValue, setDisplayValue] = React.useState<string>("");
+
+    const regx = React.useMemo(
+      () =>
+        decimalPlaces === 0
+          ? /^\d*$/
+          : new RegExp(`^\\d*\\.?\\d{0,${decimalPlaces}}$`),
+      [decimalPlaces]
+    );
+
+    const onChange = React.useCallback(
+      (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = parseNumber(event.target.value);
+
+        if (regx.test(parseNumber(value))) {
+          const formattedValue = formatNumber(value);
+          setDisplayValue(formattedValue);
+
+          props?.onChange?.({
+            ...event,
+            target: {
+              ...event.target,
+              value: parseNumber(value),
+            },
+          });
+        }
+      },
+      [min, max, regx, props?.onChange]
+    );
+
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+        value={displayValue}
+        onChange={onChange}
+      />
+    );
+  }
+);
+InputNumber.displayName = "Input";
+
+export { InputNumber };
