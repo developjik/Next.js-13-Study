621d2e47de09d1cde20135818fc096c9e4953b27
developjik <developjik@gmail.com>
2024-05-14 03:50:53 +0900

    feat: add tailwind setting to ui package

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "@repo/configs/tailwind/tailwind.config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
+  content: ["./components/**/*.{js,ts,jsx,tsx}"],
   presets: [sharedConfig],
 };
-
+ 
 export default config;
