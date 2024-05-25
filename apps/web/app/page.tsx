7fe13f979623d964167df865341050952de18e34
developjik <developjik@gmail.com>
2024-05-25 20:29:06 +0900

    feat: add @repo/configs to web package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,9 @@
-"use client";
+import React from 'react'
 
-import React from "react";
-import { Button } from "@repo/ui/src/button";
-
-function RootPage() {
+function Home() {
   return (
-    <div className="border border-soilid border-red-400 mb-4">
-      <div>RootPage</div>
-      <Button>button</Button>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
 
-export default RootPage;
+export default Home
