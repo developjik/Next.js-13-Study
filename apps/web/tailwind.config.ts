66eaac659b6854039d3a53659e0af2bfa15ed684
developjik <developjik@gmail.com>
2024-06-20 01:13:57 +0900

    chore: delete web apps

--- apps/web/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/configs/tailwind/tailwind.config";
-
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  presets: [sharedConfig],
-};
- 
-export default config;
