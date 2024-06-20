79f562f9edc15394a1f069a2219ed1af477ac3f2
developjik <developjik@gmail.com>
2024-06-21 01:34:58 +0900

    chore: delete common apps unnecessary files

---
+++ apps/common/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Common Home</div>;
+};
+
+export default App;
---
+++ apps/common/vite.config.ts
@@ -0,0 +1,24 @@
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "common",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./page": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
